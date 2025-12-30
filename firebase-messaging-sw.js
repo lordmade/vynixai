3bdfb1d579c364e680254e94c4532d540fe8ac83
Katlego Phasha <125837188+lordmade@users.noreply.github.com>
2025-12-30 08:56:35 +0200

    Update and rename dashboard.html to firebase-messaging-sw.js

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,25 @@
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/12.7.0/firebase-app.js';
+import { getMessaging, onBackgroundMessage } from 'https://www.gstatic.com/firebasejs/12.7.0/firebase-messaging-sw.js';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBOyZ3As4GTuNvjemvPF_SpsC6m6vqtNhc",
+    authDomain: "fire-b-a8878.firebaseapp.com",
+    databaseURL: "https://fire-b-a8878.firebaseio.com",
+    projectId: "fire-b-a8878",
+    storageBucket: "fire-b-a8878.firebasestorage.app",
+    messagingSenderId: "658673187627",
+    appId: "1:658673187627:web:6e4c29af661785f0afa36e"
+};
+
+const app = initializeApp(firebaseConfig);
+const messaging = getMessaging(app);
+
+onBackgroundMessage(messaging, (payload) => {
+  console.log('[firebase-messaging-sw.js] Received background message ', payload);
+  const notificationTitle = payload.notification?.title || 'New Message';
+  const notificationOptions = {
+    body: payload.notification?.body || 'You have a new message',
+    icon: '/icon-192x192.png'
+  };
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
